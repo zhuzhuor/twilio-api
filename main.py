f98a18288c39330b14e98d0b36cd23b3e117020c
Bo Zhu <bb@bozhu.me>
2013-10-03 16:45:13 -0400

    to do test first

--- main.py
+++ main.py
@@ -1,10 +1,37 @@
 __author__ = 'bozhu'
 
+
 import os
-from flask import Flask
+from flask import Flask, request, Response
+from twilio.rest import TwilioRestClient, twiml
+
 
 app = Flask(__name__)
+twilio_client = TwilioRestClient(
+    os.environ.get('ACCOUNT_SID'),
+    os.environ.get('ACCOUNT_TOKEN')
+)
 
-@app.route('/')
-def hello():
-    return 'Hello World!'
+
+@app.route('/call_me', methods=['POST'])
+def call_me():
+    return twilio_client, 'OK'
+
+
+@app.route('/handle_call', methods=['GET'])
+def handle_call():
+    resp = twiml.Response()
+    resp.play('https://api.twilio.com/cowbell.mp3', loop=10)
+    return Response(str(resp), mimetype='text/xml')
+
+
+@app.route('/text_me', methods=['POST'])
+def text_me(phone_num, text_content):
+    pass
+
+
+@app.route('/handle_text', methods=['GET'])
+def handle_text():
+    resp = twiml.Response()
+    resp.message('This is a response.')
+    return Response(str(resp), mimetype='text/xml')
